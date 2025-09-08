541fa541d1aecfda0254fe83845c9e9d97c4ab3f
Tucker McKee <tuckermckee@outlook.com>
2025-09-08 14:28:58 -0500

    moved express.static route

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const clientBuildPath = path.join(__dirname, "./client/dist");
 
-app.use(express.static(clientBuildPath));
-
-
-
 const corsOptions = {
     origin: CLIENT_URL,  //use '*' for all origins
     credentials: true,  // allow cookies to be sent in requests
@@ -28,6 +24,8 @@ app.use(express.json());
 app.use('/youtube',youtubeRoutes);
 app.use('/spotify',spotifyRoutes);
 
+app.use(express.static(clientBuildPath));
+
 // For React Router:
 app.get("*", (req, res) => {
     res.sendFile(path.join(clientBuildPath, "index.html"));
@@ -36,7 +34,6 @@ app.get("*", (req, res) => {
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Route not found' });
 });
-
 
 app.use((err, req, res, next) => {
     console.error(err.stack);

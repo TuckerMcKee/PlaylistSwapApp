ef66cedafb3f8fe10a372f23880ed1a786eaf1aa
Tucker McKee <tuckermckee@outlook.com>
2025-09-08 14:14:48 -0500

    moved express.static and catch all routes

--- server/app.js
+++ server/app.js
@@ -14,13 +14,6 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const clientBuildPath = path.join(__dirname, "../client/dist");
 
-app.use(express.static(clientBuildPath));
-
-// For React Router:
-app.get("*", (req, res) => {
-  res.sendFile(path.join(clientBuildPath, "index.html"));
-});
-
 const corsOptions = {
     origin: CLIENT_URL,  //use '*' for all origins
     credentials: true,  // allow cookies to be sent in requests
@@ -30,6 +23,13 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/youtube',youtubeRoutes);
 app.use('/spotify',spotifyRoutes);
+
+app.use(express.static(clientBuildPath));
+
+// For React Router:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
+});
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Route not found' });

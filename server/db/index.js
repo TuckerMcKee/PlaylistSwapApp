8444cdae593567aac8007f6f0d8496d6a21bc692
Tucker McKee <tuckermckee@outlook.com>
2025-09-16 13:30:39 -0500

    enabling tls for prod

--- server/db/index.js
+++ server/db/index.js
@@ -3,8 +3,17 @@ import { DATABASE_URL } from '../config/index.js';
 
 const { Pool } = pkg;
 
+const shouldEnableSSL =
+  process.env.NODE_ENV === 'production' ||
+  (DATABASE_URL && !/localhost|127\.0\.0\.1/.test(DATABASE_URL));
+
 const pool = new Pool({
   connectionString: DATABASE_URL,
+  ...(shouldEnableSSL
+    ? {
+        ssl: { rejectUnauthorized: false },
+      }
+    : {}),
 });
 
 export default pool;
